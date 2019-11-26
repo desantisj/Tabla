4618a7503e324a3196860c363eff1640693b7def
desantij <josephdesantis07@gmail.com>
2019-11-26 12:58:36 -0500

    new update

--- tabla.py
+++ tabla.py
@@ -1,7 +1,9 @@
 import serial as ser
 import bluetooth
-#from colour_follow.py import colour_track()
 import numpy as np
+from imutils.video import VideoStream
+import imutils
+import RPi.GPIO as GPIO
 import cv2
 import os
 from time import sleep
@@ -33,7 +35,7 @@ def main_input():
 def mode1():
         
 	check = '0'
-        print('mode1')
+        print('Mode 1')
         
 	while(check!= 'q'):
                 recvdata = client_sock.recv(1024)
@@ -61,109 +63,146 @@ def mode2():
         mode2_var = '0'
 
         while(mode2_var!= 'q'):
-                #Capture from external USB webcam instead of the in-built webcam (shitty quality)
-                cap = cv2.VideoCapture(0)
+            # construct the argument parse and parse the arguments
+            ap = argparse.ArgumentParser()
+            ap.add_argument("-v", "--video",
+                    help="path to the (optional) video file")
+            ap.add_argument("-b", "--buffer", type=int, default=32,
+                    help="max buffer size")
+            args = vars(ap.parse_args())
 
-                #kernel window for morphological operations
-                kernel = np.ones((5,5),np.uint8)
+            # initialize the list of tracked points, the frame counter,
+            # and the coordinate deltas
+            pts = deque(maxlen=args["buffer"])
+            counter = 0
+            (dX, dY) = (0, 0)
+            direction = ""
 
-                #resize the capture window to 640 x 480
-                ret = cap.set(3,640)
-                ret = cap.set(4,480)
+            # if a video path was not supplied, grab the reference
+            # to the webcam
+            if not args.get("video", False):
+                    vs = VideoStream(src=0).start()
 
-                #upper and lower limits for the color yellow in HSV color space
-                lower_yellow = np.array([20,100,100])
-                upper_yellow = np.array([30,255,255])
+            # otherwise, grab a reference to the video file
+            else:
+                    vs = cv2.VideoCapture(args["video"])
 
-                #begin capture
-                while(True):
-                    recvdata = client_sock.recv(1024)
-                    mode2_var = recvdata
-                
-                    ret, frame = cap.read()
+            # allow the camera or video file to warm up
+            time.sleep(2.0)
 
-                    #Smooth the frame
-                    frame = cv2.GaussianBlur(frame,(11,11),0)
+            # keep looping
+            while True:
+                    mode2_var = client_sock.recv(1024)
+                    
+                    dist = distance()
+                    
+                    # grab the current frame
+                    frame = vs.read()
 
-                    #Convert to HSV color space
-                    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+                    # handle the frame from VideoCapture or VideoStream
+                    frame = frame[1] if args.get("video", False) else frame
 
-                    #Mask to extract just the yellow pixels
-                    mask = cv2.inRange(hsv,lower_yellow,upper_yellow)
+                    # if we are viewing a video and we did not grab a frame,
+                    # then we have reached the end of the video
+                    if frame is None:
+                            break
 
-                    #morphological opening
-                    mask = cv2.erode(mask,kernel,iterations=2)
-                    mask = cv2.dilate(mask,kernel,iterations=2)
+                    # resize the frame, blur it, and convert it to the HSV
+                    # color space
+                    frame = imutils.resize(frame, width=600)
+                    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+                    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-                    #morphological closing
-                    mask = cv2.dilate(mask,kernel,iterations=2)
-                    mask = cv2.erode(mask,kernel,iterations=2)
+                    # construct a mask for the color "green", then perform
+                    # a series of dilations and erosions to remove any small
+                    # blobs left in the mask
+                    
+                    lower_red = np.array([170,120,70])
+                    upper_red = np.array([180,255,255])
+                    mask1 = cv2.inRange(hsv,lower_red,upper_red)
+                    
+                    # Range for upper range
+                    
+                    # Range for lower red
+                    lower_red = np.array([0,120,70])
+                    upper_red = np.array([10,255,255])
+                    
+                    mask2 = cv2.inRange(hsv,lower_red,upper_red)
+                     
+                    # Generating the final mask to detect red color
+                    mask = mask1+mask2
+                    mask = cv2.erode(mask, None, iterations=2)
+                    mask = cv2.dilate(mask, None, iterations=2)
 
-                    #Detect contours from the mask
+                    # find contours in the mask and initialize the current
+                    # (x, y) center of the ball
                     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                                cv2.CHAIN_APPROX_SIMPLE)[-2]
+                            cv2.CHAIN_APPROX_SIMPLE)
+                    cnts = imutils.grab_contours(cnts)
+                    center = None
 
-                    if(len(cnts) > 0):
-                        #Contour with greatest area
-                        c = max(cnts,key=cv2.contourArea)
-                        #Radius and center pixel coordinate of the largest contour
-                        ((x,y),radius) = cv2.minEnclosingCircle(c)
+                    # only proceed if at least one contour was found
+                    if len(cnts) > 0:
+                            # find the largest contour in the mask, then use
+                            # it to compute the minimum enclosing circle and
+                            # centroid
+                            c = max(cnts, key=cv2.contourArea)
+                            ((x, y), radius) = cv2.minEnclosingCircle(c)
+                            M = cv2.moments(c)
+                            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-                        if radius > 5:
-                            #Draw an enclosing circle
-                            cv2.circle(frame,(int(x), int(y)), int(radius),(0, 255, 255), 2)
-
+                            # only proceed if the radius meets a minimum size
+                            if radius > 10:
+                                    # draw the circle and centroid on the frame,
+                                    # then update the list of tracked points
+                                    cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+                                    cv2.circle(frame, center, 5, (0, 0, 255), -1)
                             #Draw a line from the center of the frame to the center of the contour
-                            cv2.line(frame,(320,240),(int(x), int(y)),(0, 0, 255), 1)
+                            cv2.line(frame,(300,300),(int(x), int(y)),(0, 0, 255), 1)
                             #Reference line
-                            cv2.line(frame,(320,0),(320,480),(0,255,0),1)
+                            cv2.line(frame,(300,0),(300,600),(0,255,0),1)
 
                             radius = int(radius)
 
                             #distance of the 'x' coordinate from the center of the frame
                             #wdith of frame is 640, hence 320
-                            length = 320-(int(x))
+                            length = 300-(int(x))
 
-                            if length > 150:#turn left
-                                    moveleft()
-                                    print('left')
-                                    
-                            elif length < -150:#turn right
-                                    moveright()
-                                    print('right')
-
-                            elif length <150 or length >-150:#move forward
-                                    moveforward()
-                                    print('forward')
-
-                            else:
+                            if dist < 75:
                                     motorstop()
-                                    print('stop')
-                                    sleep(0.01)
+                                    print('stop -- ', length, dist)
 
-                        else:
-                            motorstop()
-                            sleep(0.01)
+                            elif length > 200:#turn left
+                                    moveleft()
+                                    print('left -- ',length)
+                                    
+                            elif length < -200:#turn right
+                                    moveright()
+                                    print('right -- ', length)
 
-                        if mode2_var == 'q':
-                                break
+                            elif length <= 200 and length >= -200:
+                            #move forward
+                                    moveforward()
+                                    print('forward -- ', length)
+
+                            else :
+                                print("stop1")
+                                motorstop()
+                                sleep(0.01)
 
 
-                    #display the image
-                    cv2.imshow('frame',frame)
-                    #Mask image
-                    cv2.imshow('mask',mask)
+            ##	# show the frame to our screen and increment the frame counter
+                    cv2.imshow("Frame", frame)
+                    key = cv2.waitKey(1) & 0xFF
+
+            # if we are not using a video file, stop the camera video stream
+            if not args.get("video", False):
+                    vs.stop()
+
+            # otherwise, release the camera
+            else:
+                    vs.release()
             
-        motorstop()
-        #Release the capture
-        cap.release()
-        cv2.destroyAllWindows()
-          
-                	
-def mode3():
-	print("Mode 3")
-	input()
-	
 def quit_():
 	print("quitting program")
 	direction = "1"
@@ -177,42 +216,47 @@ def quit_():
 	pwm_signal = "0"
 	set_state(direction, state, pwm, pwm_signal)
 	server_sock.close()
+	GPIO.cleanup()
 	
 	sys.exit()
 	
 def moveforward():
-        print ("forward")
+        print("forward")
         state = "0"
-	pwm_signal = "15"
-	direction = "1"
-	pwm = "1"
-	set_state(direction, state, pwm, pwm_signal)
-	direction = "2"
-	pwm = "2"
-	set_state(direction, state, pwm, pwm_signal)
+        pwm_signal = "35"
+        direction = "1"
+        pwm = "1"
+        set_state(direction, state, pwm, pwm_signal)
+        state = "0"
+        pwm_signal = "28"
+        direction = "2"
+        pwm = "2"
+        set_state(direction, state, pwm, pwm_signal)
 	
 
 def movebackward():
-        print ("backward")
+        print("backward")
         state = "1"
-	pwm_signal = "15"
-	direction = "1"
-	pwm = "1"
-	set_state(direction, state, pwm, pwm_signal)
-	direction = "2"
-	pwm = "2"
-	set_state(direction, state, pwm, pwm_signal)
+        pwm_signal = "35"
+        direction = "1"
+        pwm = "1"
+        set_state(direction, state, pwm, pwm_signal)
+        state = "1"
+        pwm_signal = "28"
+        direction = "2"
+        pwm = "2"
+        set_state(direction, state, pwm, pwm_signal)
 	
 
 def moveright():
         print ("right")
         state = "1"
-	pwm_signal = "15"
+	pwm_signal = "22"
 	direction = "1"
 	pwm = "1"
 	set_state(direction, state, pwm, pwm_signal)
 	state = "0"
-	pwm_signal = "15"
+	pwm_signal = "22"
 	direction = "2"
 	pwm = "2"
 	set_state(direction, state, pwm, pwm_signal)
@@ -221,12 +265,12 @@ def moveright():
 def moveleft():
         print ("left")
         state = "0"
-	pwm_signal = "15"
+	pwm_signal = "22"
 	direction = "1"
 	pwm = "1"
 	set_state(direction, state, pwm, pwm_signal)
 	state = "1"
-	pwm_signal = "15"
+	pwm_signal = "22"
 	direction = "2"
 	pwm = "2"
 	set_state(direction, state, pwm, pwm_signal)
@@ -245,10 +289,48 @@ def motorstop():
 	pwm_signal = "0"
 	set_state(direction, state, pwm, pwm_signal)
 
+def distacne():
+        # set Trigger to HIGH
+        GPIO.output(GPIO_TRIGGER, True)
+     
+        # set Trigger after 0.01ms to LOW
+        time.sleep(0.0001)
+        GPIO.output(GPIO_TRIGGER, False)
+     
+        StartTime = time.time()
+        StopTime = time.time()
+     
+        # save StartTime
+        while GPIO.input(GPIO_ECHO) == 0:
+            StartTime = time.time()
+     
+        # save time of arrival
+        while GPIO.input(GPIO_ECHO) == 1:
+            StopTime = time.time()
+     
+        # time difference between start and arrival
+        TimeElapsed = StopTime - StartTime
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance = (TimeElapsed * 34300) / 2
+     
+        return distance
+
 if __name__ == "__main__":
         print("waiting for connection in main")
 	port = '/dev/ttyACM0'
         s = ser.Serial(port,9600,timeout=5)
+
+        GPIO.setmode(GPIO.BCM)
+
+        #set GPIO Pins
+        GPIO_TRIGGER = 18
+        GPIO_ECHO = 24
+
+        #set GPIO direction (IN / OUT)
+        GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+        GPIO.setup(GPIO_ECHO, GPIO.IN)
+    
 	server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 	port = 1
 	server_sock.bind(("",port))
